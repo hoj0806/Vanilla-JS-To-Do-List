857bb15adb5a5efb6d2e87e1c0ed7c998eec117b
홍재훈 <ehswkfn123@naver.com>
2024-10-27 21:12:30 +0900

    feature : 팝업창 확인 버튼 클릭시 팝업창 사라지게 하는 기능추가

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,13 @@
 const listAddBtn = document.querySelector(".feature-box__add-button-box");
 const popupBox = document.querySelector(".pop-up-box");
 const addInput = document.querySelector(".add-input");
-
+const popupBtn = document.querySelector(".pop-up-confirm-btn");
+// Event handler
 listAddBtn.addEventListener("click", () => {
   popupBox.classList.add("show-pop-up");
+});
+
+popupBtn.addEventListener("click", () => {
+  console.log(addInput.value);
+  popupBox.classList.remove("show-pop-up");
 });
