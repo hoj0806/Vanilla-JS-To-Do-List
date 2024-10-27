f03ac2ecc1a9535815f2a0d86d5d353b111b52ec
홍재훈 <ehswkfn123@naver.com>
2024-10-27 21:04:14 +0900

    ui : 팝업창 기본 디자인

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 // selectors
 const listAddBtn = document.querySelector(".feature-box__add-button-box");
 const popupBox = document.querySelector(".pop-up-box");
+const addInput = document.querySelector(".add-input");
+
 listAddBtn.addEventListener("click", () => {
   popupBox.classList.add("show-pop-up");
 });
