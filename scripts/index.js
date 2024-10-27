8f37ec092b0a303d0e35b4792bf7cfb33a36493e
홍재훈 <ehswkfn123@naver.com>
2024-10-27 21:28:23 +0900

    feature : 할 일 목록 입력후 확인시 리스트에 추가되는 기능 추가

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,30 @@ const listAddBtn = document.querySelector(".feature-box__add-button-box");
 const popupBox = document.querySelector(".pop-up-box");
 const addInput = document.querySelector(".add-input");
 const popupBtn = document.querySelector(".pop-up-confirm-btn");
+const listContainer = document.querySelector(".todo-list-container");
+const listItems = document.querySelectorAll(".list-item");
+
+let todoList = [];
+
+listItems.forEach((item) => {
+  todoList.push({
+    title: item.innerHTML,
+    isFinished: false,
+  });
+});
+
 // Event handler
 listAddBtn.addEventListener("click", () => {
   popupBox.classList.add("show-pop-up");
 });
 
 popupBtn.addEventListener("click", () => {
-  console.log(addInput.value);
+  todoList.push({
+    title: addInput.value,
+    isFinished: false,
+  });
   popupBox.classList.remove("show-pop-up");
+  console.log(todoList);
+  const html = `<li class="list-item">${addInput.value}</li>`;
+  listContainer.insertAdjacentHTML("beforeend", html);
 });
