414e83d5cb18eb20d12339652e81327e7ceb62de
홍재훈 <ehswkfn123@naver.com>
2024-10-29 19:16:56 +0900

    feature : 할 일 읽기 팝업창에 메모기능 추가

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,15 @@ const popupBtn = document.querySelector(".pop-up-confirm-btn");
 const popupTitle = document.querySelector(".pop-up-title");
 const editAndAddPopup = document.querySelector(".pop-up-edit-and-add");
 const readPopup = document.querySelector(".pop-up-read");
+const popupReadTitle = document.querySelector(".pop-up-read-title");
+const popupReadConfirmBtn = document.querySelector(".pop-up-confirm-btn-read");
 
 const listAddBtn = document.querySelector(".feature-box__add-button-box");
 const addInput = document.querySelector(".add-input");
 const listContainer = document.querySelector(".todo-list-container");
-
+const memoBtn = document.querySelector(".memo-button");
+const memoInput = document.querySelector(".memo-input");
+const memoContent = document.querySelector(".memo-content");
 let selectList;
 
 // Event handler
@@ -65,8 +69,30 @@ listContainer.addEventListener("click", (e) => {
   }
 
   if (e.target.classList.contains("list-title")) {
+    selectList = e.target;
+    memoContent.textContent = e.target.dataset.memo;
     popupWrapper.classList.add("show-pop-up");
     editAndAddPopup.classList.add("hide-pop-up");
     readPopup.classList.remove("hide-pop-up");
+    popupReadTitle.textContent = e.target.textContent;
   }
+});
+
+memoBtn.addEventListener("click", () => {
+  if (memoBtn.textContent === "확인") {
+    memoInput.classList.remove("show-pop-up");
+    memoBtn.textContent = "메모하기";
+    selectList.setAttribute("data-memo", memoInput.value);
+    memoContent.textContent = selectList.dataset.memo;
+    // console.log(memoInput.value);
+  } else {
+    memoBtn.textContent = "확인";
+    memoInput.classList.add("show-pop-up");
+  }
+});
+
+popupReadConfirmBtn.addEventListener("click", () => {
+  memoContent.textContent = "";
+  memoInput.value = "";
+  popupWrapper.classList.remove("show-pop-up");
 });
